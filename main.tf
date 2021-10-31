b9f76a3d59a38b5c1c0f9c8ef30cc7a08c378f85
ldmkor <leedm.mobile@gmail.com>
2021-10-31 23:29:14 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "terraform-intersight-iks" {
 
   ip_pool = {
     use_existing        = false
-    name                = "sbippool"
+    name                = "demoippool"
     ip_starting_address = "10.10.20.170"
     ip_pool_size        = "20"
     ip_netmask          = "255.255.255.0"
@@ -32,7 +32,7 @@ module "terraform-intersight-iks" {
 
   sysconfig = {
     use_existing = false
-    name         = "New"
+    name         = "network"
     domain_name  = "demo.intra"
     timezone     = "America/New_York"
     ntp_servers  = ["10.101.128.15"]
@@ -113,7 +113,7 @@ module "terraform-intersight-iks" {
   }
   # Cluster information
   cluster = {
-    name                = "sbcluster"
+    name                = "democluster"
     action              = "Deploy"
     wait_for_completion = false
     worker_nodes        = 1
