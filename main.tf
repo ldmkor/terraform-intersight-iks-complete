78af9c1999a55631a4bf607fd0dd25cad08161b9
ldmkor <leedm.mobile@gmail.com>
2021-10-31 21:37:49 +0900

    Commit to Variables to variables.tf

--- main.tf
+++ main.tf
@@ -125,37 +125,6 @@ module "terraform-intersight-iks" {
   }
   # Organization and Tag
   organization = var.organization
-  tags         = var.tags
-}
 
-variable "apikey" {
-  type        = string
-  description = "API Key"
-}
-  
-variable "secretkey" {
-  type        = string
-  description = "Secret Key or file location"
-}
-  
-variable "organization" {
-  type        = string
-  description = "Organization Name"
-  default     = "default"
-}
-
-variable "vc_password" {
-  type        = string
-  description = "Secret Key or file location"
-}  
-
-variable "ssh_user" {
-  type        = string
-  description = "SSH user name to be used to node login."
-}
-  
-variable "ssh_key" {
-  type        = string
-  description = "SSH Public Key to be used to node login."
 }
   
