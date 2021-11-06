0feb811a151ef64dec5b8a1a16687b759299536a
ldmkor <leedm.mobile@gmail.com>
2021-11-07 00:03:13 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ module "terraform-intersight-iks" {
   # Cluster information
   cluster = {
     name                = "tfc-iks-cluster"
-    action              = "Deploy"
+    action              = "Unassign"
     wait_for_completion = false
     worker_nodes        = 1
     load_balancers      = 1
